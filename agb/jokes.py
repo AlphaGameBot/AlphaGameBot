b713eb4e87785573896738109c7161097e80e427
Damien Boisvert <damienboisvert8@gmail.com>
2024-01-22 16:55:04 -0800

    Initial commit from my PC
    Hey yo

---
+++ agb/jokes.py
@@ -0,0 +1,27 @@
+import logging
+
+import discord
+from discord.ext import commands
+from . import requestHandler
+import json
+from nltk.corpus import words
+import logging
+import random
+
+class jokesCog(discord.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = logging.getLogger("cogwheel")
+        self.logger.info("jokesCog has been initalized.")
+
+    @commands.slash_command(name="joke", description="I'm so funny, right?")
+    async def _joke(self, interaction: discord.Interaction):
+        r = requestHandler.handler.get("https://official-joke-api.appspot.com/random_joke", attemptCache=False)
+        joke = json.loads(r.text)
+
+        embed = discord.Embed(title="Joke", description="{0}\n{1}".format(joke["setup"], joke["punchline"]))
+        await interaction.response.send_message(embed=embed)
+
+    @commands.slash_command(name="wisdom", description="Get a word of wisdom")
+    async def _wisdom(self, interaction: discord.Interaction):
+        await interaction.response.send_message(random.choice(words()))
---
+++ main.py
@@ -0,0 +1,71 @@
+import discord
+from discord.ext import commands
+import os
+import nltk
+import logging
+import logging.config
+import agb.cogwheel
+import threading
+import webui.app as webui
+# commands
+import agb.utility
+import agb.xkcd
+import agb.memes
+import agb.jokes
+
+# if you wanna set custom logging configs i guess
+# this is in .gitignore and .dockerignore because
+# not everyone needs it, and if they do, it will
+# be automatically loaded. :) --Damien 12.22.23
+# TODO: Add base logging.cfg for people to copy
+logging.config.fileConfig("logging.ini")
+#logging.basicConfig(level=logging.DEBUG)
+intents = discord.Intents.all()
+
+DAMIEN = 420052952686919690
+
+#if os.getenv("DEBUG") != None:
+#    logging.basicConfig(level=logging.DEBUG)
+#else:
+#    logging.basicConfig(level=logging.INFO)
+
+bot = commands.Bot(command_prefix="?", intents=intents)
+nltk.download('words')
+@bot.event
+async def on_ready():
+    status = discord.Game("with the API")
+    await bot.change_presence(activity=status)
+    bot.auto_sync_commands = True
+    print("Bot is ready! :)")
+
+@bot.event
+async def on_application_command_error(interaction: discord.Interaction, error: discord.DiscordException):
+    embed = agb.cogwheel.embed(title="An error occured...", description="An internal server error has occured, and the bot cannot fulfill your request.  You may be able \
+                                                                   to make it work by trying again.\nSorry about that! (awkward face emoji)", color=discord.Color.red())
+
+
+    embed.set_thumbnail(url="https://static.alphagame.dev/alphagamebot/img/error.png")
+    await interaction.response.send_message(embed=embed)
+
+
+@bot.command(name="say")
+async def _say(ctx: discord.ext.commands.context.Context, *, text:str=None):
+    if ctx.author.id != DAMIEN:
+        print("{0} tried to make me say \"{1}\", but I successfully ignored it.")
+        await ctx.send(":x: I beg your pardon, but my creator only wants me to say his opinions.")
+        return
+    if text == None:
+        return
+    print("I was told to say: \"{}\".".format(text))
+    await ctx.send(text)
+    await ctx.message.delete()
+
+# set command cogs
+bot.add_cog(agb.utility.UtilityCog(bot))
+bot.add_cog(agb.xkcd.xkcdCog(bot))
+bot.add_cog(agb.memes.MemesCog(bot))
+bot.add_cog(agb.jokes.jokesCog(bot))
+
+if __name__ == "__main__":
+    threading.Thread(target=webui.start).start()
+    bot.run(os.getenv("TOKEN"))
