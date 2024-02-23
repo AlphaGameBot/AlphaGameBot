4c6bc63bcdd9fac0105c1fb3422af237275ab65c
Damien Boisvert <damienboisvert8@gmail.com>
2024-02-23 11:19:57 -0800

    Finished Rock, Paper, Scissors command!

--- agb/rps.py
+++ agb/rps.py
@@ -4,6 +4,7 @@ import agb.cogwheel
 import logging
 import random
 
+
 class rpsCog(discord.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -27,20 +28,20 @@ class rpsCog(discord.Cog):
     def getWinnerState(self, player_choice, computer_choice):
         self.logger.debug("getWinnerState was called with values {}".format((player_choice, computer_choice)))
         data = {
-            "winner": -1, # the number
+            "winner": -1,  # the number
             "human-win-status": "<ERROR:UNCHANGED VALUE>",
             "players": {
                 "computer": computer_choice,
                 "player": player_choice
             }
         }
-        
+
         if player_choice == computer_choice:
             data["winner"] = 2
             data["human-win-status"] = "TIED"
         elif (player_choice == "rock" and computer_choice == "scissors") or \
-            (player_choice == "paper" and computer_choice == "rock") or \
-            (player_choice == "scissors" and computer_choice == "paper"):
+                (player_choice == "paper" and computer_choice == "rock") or \
+                (player_choice == "scissors" and computer_choice == "paper"):
             data["winner"] = 0
             data["human-win-status"] = "WON"
         else:
@@ -49,26 +50,25 @@ class rpsCog(discord.Cog):
         return data
 
     @commands.slash_command(name="rps", description="Let's play Rock Paper Scissors!")
-    async def _rps(self, interaction: discord.Interaction, playerchoice:str):
+    async def _rps(self, interaction: discord.Interaction, playerchoice: str):
         if playerchoice.lower() not in self.gameData.keys():
-            # well shit i guess you entered something wrong :/
-            embed = agb.cogwheel.embed(title="Error!", description="Well this is awkward... '{}' doesn't seem to be a valid option.  Please use:\nrock,paper,scissors".format(playerchoice))
+            # well shit I guess you entered something wrong :/
+            embed = agb.cogwheel.embed(title="Error!",
+                                       description="Well this is awkward... '{}' doesn't seem to be a valid option.  Please use:\nrock,paper,scissors".format(
+                                           playerchoice))
             await interaction.response.send_message(embed=embed)
             return None
         ai = random.choice(["rock", "paper", "scissors"])
-        pl = playerchoice.lower() # player's choice
-
+        pl = playerchoice.lower()  # player's choice
         state = self.getWinnerState(pl, ai)
         # 1 --> Player wins
         # 2 --> AI wins
         # 0 --> Draw
 
         embed = agb.cogwheel.embed(title="Rock Paper Scissors",
-                               description=(
-                                   "You %s" % state["human-win-status"]))
+                                   description=(
+                                           "You %s" % state["human-win-status"]))
         embed.add_field(name="Your choice", value=state["players"]["player"])
         embed.add_field(name="Bot's choice", value=state["players"]["computer"])
 
         await interaction.response.send_message(embed=embed)
-    
-    
