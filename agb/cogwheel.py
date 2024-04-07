a7a07d169616e87ffd8ba4cd6a6171d5f56cd9e1
Damien Boisvert <damien@alphagame.dev>
2024-04-06 20:19:48 -0700

    Add GPL License text to top of files

--- agb/cogwheel.py
+++ agb/cogwheel.py
@@ -1,3 +1,19 @@
+#    AlphaGameBot - A Discord bot that's free and (hopefully) doesn't suck
+#    Copyright (C) 2024  Damien Boisvert (AlphaGameDeveloper)
+
+#    AlphaGameBot is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    AlphaGameBot is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+
+#    You should have received a copy of the GNU General Public License
+#    along with AlphaGameBot.  If not, see <https://www.gnu.org/licenses/>.
+
 import discord
 from discord.embeds import *
 import os
