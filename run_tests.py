9b373dc0de9d031c56f4143c907e14e673b2c842
Damien Boisvert <damien@alphagame.dev>
2024-09-15 23:08:11 -0700

    Actually fail if tests fail

--- run_tests.py
+++ run_tests.py
@@ -1,9 +1,11 @@
 import unittest
-
+import sys
 import agb.tests as tests
 
 suite = unittest.TestLoader().discover("agb/tests/", pattern="*.py")
 
 if __name__ == "__main__":
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
  
+    if result.errors or result.failures:
+        exit(1)
